087ddafe7d2c01b6d300651dd29f9e6c74ddb74c
vinay pandey <abhi837688@gmail.com>
2025-03-31 12:13:08 +0530

    animation delet

--- script.js
+++ script.js
@@ -17,28 +17,3 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     search.classList.remove('active');
 }
-
-const animate = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2500,
-    delay: 400,
-})
-
-animate.reveal('.logo,.header-icon,#menu-icon');
-animate.reveal('ul',{origin:"top"})
-animate.reveal(".home-img img", {dealy:400,origin: "right"});
-animate.reveal(".home-text", {origin: "left"});
-
-
-// about
-animate.reveal(".about-img img", {origin: "left"});
-animate.reveal(".about-text", {origin: "right"});
-
-// card
-animate.reveal(".box", {interval: 100});
-animate.reveal(".heading", {origin: "top"});
-
-// footer
-animate.reveal(".footer",{origin:"bottom"})
-animate.reveal(".copy",{origin:"bottom"})
